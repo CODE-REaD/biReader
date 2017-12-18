d37126f172dd3e2430cc625c90faec779af52b76
code-read <github@carsonwilson.com>
2017-12-18 11:50:15 -0600

    now speaks clicked text

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ document.getElementById('filechoice2')
                 document.getElementById('rightTitle').textContent = this.files[0].name;
             }
         );
+
+$(".clickable").click(function(e){
+    var s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
+    while(range.toString().indexOf(' ') != 0) {
+        range.setStart(node,(range.startOffset -1));
+    }
+    range.setStart(node, range.startOffset +1);
+
+    do {
+        range.setEnd(node,range.endOffset + 1);
+
+    } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
+    var str = range.toString().trim();
+    alert(str);
+
+/*
+    var can = this.hasSpeechError()
+    if (can) {
+        console.log(this.props)
+        this.props.onSpeechError(can)
+        return;
+    }
+*/
+    var msg = new SpeechSynthesisUtterance(str);
+    // msg.lang = this.props.language;
+    msg.lang = 'fr';
+    msg.rate = 0.8;
+    speechSynthesis.speak(msg)
+
+});
+
+
