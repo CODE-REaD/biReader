277be29417f6450843b921dfd5a2d647719e1217
code-read <github@carsonwilson.com>
2018-01-11 09:52:50 -0600

    Proofing continues: improved word selection code.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,55 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
 
 // todo: Apply D.R.Y. here...
-var leftFileDrop = document.getElementById("leftColumn"); // use column rather than para as para isn't inflated before a file is loaded
-leftFileDrop.addEventListener("dragenter", dragenter, false);
-leftFileDrop.addEventListener("dragover", dragover, false);
-leftFileDrop.addEventListener("drop", leftDrop, false);
-leftFileDrop.addEventListener("paste", leftPaste, false);
 
-var rightFileDrop = document.getElementById("rightColumn");
-rightFileDrop.addEventListener("dragenter", dragenter, false);
-rightFileDrop.addEventListener("dragover", dragover, false);
-rightFileDrop.addEventListener("drop", rightDrop, false);
-rightFileDrop.addEventListener("paste", rightPaste, false); //todo: this replaces leftFileDrop.addEventListener (above).  Why?
+// When user makes a change to the 'filechoice1' field, fire this listener to load
+// the file to leftPara:
+document.getElementById('leftFileChoice')
+    .addEventListener(
+        'change',
+        function () {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById('leftPara').textContent = this.result;
+            };
+            fr.readAsText(this.files[0]);
+            document.getElementById('leftTitle').textContent = this.files[0].name;
+            updateLineSpacing();
+        }
+    );
+
+document.getElementById('rightFileChoice')
+    .addEventListener(
+        'change',
+        function () {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById('rightPara').textContent = this.result;
+            };
+            fr.readAsText(this.files[0]);
+            document.getElementById('rightTitle').textContent = this.files[0].name;
+            updateLineSpacing();
+        }
+    );
+
+var leftFileColumn = document.getElementById("leftColumn"); // use column rather than para as para isn't inflated before a file is loaded
+leftFileColumn.addEventListener("dragenter", dragenter, false);
+leftFileColumn.addEventListener("dragover", dragover, false);
+leftFileColumn.addEventListener("drop", leftDrop, false);
+leftFileColumn.addEventListener("paste", leftPaste, false);
+
+var rightFileColumn = document.getElementById("rightColumn");
+rightFileColumn.addEventListener("dragenter", dragenter, false);
+rightFileColumn.addEventListener("dragover", dragover, false);
+rightFileColumn.addEventListener("drop", rightDrop, false);
+rightFileColumn.addEventListener("paste", rightPaste, false);
+
+function updateLineSpacing() {
+    console.log('Change to a column detected.  Updating line spacing.');
+    var leftLength = document.getElementById('leftPara').textContent.length;
+    var rightLength = document.getElementById('rightPara').textContent.length;
+    console.log('Left length = ' + leftLength + ', Right length = ' + rightLength);
+}
 
 function dragenter(e) {
     e.stopPropagation();
@@ -28,52 +66,37 @@ function dragover(e) {
 function leftDrop(e) {
     e.stopPropagation();
     e.preventDefault();
-
     var dt = e.dataTransfer;
     var files = dt.files;
-
     handleLeftFiles(files);
 }
 
 function rightDrop(e) {
     e.stopPropagation();
     e.preventDefault();
-
     var dt = e.dataTransfer;
     var files = dt.files;
-
     handleRightFiles(files);
 }
 
-// todo: for some reason this is never triggered: only rightPaste:
 function leftPaste(e) {
     var clipboardData, pastedData;
     e.stopPropagation();
     e.preventDefault();
-
-    // Get pasted data via clipboard API
-    // clipboardData = e.clipboardData || window.clipboardData;
     clipboardData = e.clipboardData;
     pastedData = clipboardData.getData('Text');
-
-    // Do whatever with pasteddata
-    alert(pastedData);
     document.getElementById('leftPara').textContent = pastedData;
+    updateLineSpacing();
 }
 
 function rightPaste(e) {
     var clipboardData, pastedData;
     e.stopPropagation();
     e.preventDefault();
-
-    // Get pasted data via clipboard API
-    // clipboardData = e.clipboardData || window.clipboardData;
     clipboardData = e.clipboardData;
     pastedData = clipboardData.getData('Text');
-
-    // Do whatever with pasteddata
-    alert(pastedData);
     document.getElementById('rightPara').textContent = pastedData;
+    updateLineSpacing();
 }
 
 function handleLeftFiles(files) {
@@ -81,6 +104,7 @@ function handleLeftFiles(files) {
     var fr = new FileReader();
     fr.onload = function () {
         document.getElementById('leftPara').textContent = this.result;
+        updateLineSpacing();
     };
     fr.readAsText(files[0]);
     document.getElementById('leftTitle').textContent = files[0].name;
@@ -91,36 +115,11 @@ function handleRightFiles(files) {
     var fr = new FileReader();
     fr.onload = function () {
         document.getElementById('rightPara').textContent = this.result;
+        updateLineSpacing();
     };
     fr.readAsText(files[0]);
     document.getElementById('rightTitle').textContent = files[0].name;
 }
-
-document.getElementById('filechoice1')
-    .addEventListener(
-        'change',
-        function () {
-            var fr = new FileReader();
-            fr.onload = function () {
-                document.getElementById('leftPara').textContent = this.result;
-            };
-            fr.readAsText(this.files[0]);
-            document.getElementById('leftTitle').textContent = this.files[0].name;
-        }
-    );
-
-document.getElementById('filechoice2')
-    .addEventListener(
-        'change',
-        function () {
-            var fr = new FileReader();
-            fr.onload = function () {
-                document.getElementById('rightPara').textContent = this.result;
-            };
-            fr.readAsText(this.files[0]);
-            document.getElementById('rightTitle').textContent = this.files[0].name;
-        }
-    );
 
 // .. apply D.R.Y. above
 
@@ -130,50 +129,28 @@ var readTextAloud = function () {
     var range = s.getRangeAt(0);
     var node  = s.anchorNode;
 
-    // Move back to letter after first ' ':
-    // fixed bug: crashes if run from first word in text (no preceding space).
-    // todo: this fails if word is at start of paragraph (no preceding space, backs up to
-    // preceding line).
+    if (speechSynthesis.pending)
+        speechSynthesis.cancel(); // try to address "freezing"; see https://stackoverflow.com/questions/21947730/chrome-speech-synthesis-with-longer-texts
 
-/*
-    while (range.startOffset > 0 && range.toString()[0].match(/\w/))
-    {
-        range.setStart(range.startContainer, range.startOffset - 1);
+
+    while (range.startOffset !== 0) {                   // start of node
+        range.setStart(node, range.startOffset - 1)     // back up 1 char
+        if (range.toString().search(/\s/) === 0) {      // space character
+            range.setStart(node, range.startOffset + 1);// move forward 1 char
+            break;
+        }
     }
 
-    while (range.endOffset < range.endContainer.length &&
-           range.toString()[range.toString().length - 1].match(/\w/))
-    {
-        range.setEnd(range.endContainer, range.endOffset + 1);
+    while (range.endOffset < node.length) {         // end of node
+        range.setEnd(node, range.endOffset + 1)     // forward 1 char
+        if (range.toString().search(/\s/) !== -1) { // space character
+            range.setEnd(node, range.endOffset - 1);// back 1 char
+            break;
+        }
     }
-*/
-
-
-    while ((range.toString().indexOf(' ') !== 0)) {
-        if (range.StartOffset === 0) break;
-        range.setStart(node, (range.startOffset - 1))
-    }
-    range.setStart(node, range.startOffset + 1);
-
-    //
-    do {
-        range.setEnd(node, range.endOffset + 1);
-    } while (range.toString().indexOf(' ') === -1 &&
-    range.toString().trim() !== '');
 
     var str = range.toString().trim();
-    // alert(str);
-
-    /*
-        var can = this.hasSpeechError()
-        if (can) {
-            console.log(this.props)
-            this.props.onSpeechError(can)
-            return;
-        }
-    */
     var msg = new SpeechSynthesisUtterance(str);
-    // msg.lang = this.props.language;
 
     // Bad for performance: run once after new file load: for proof-of-concept code only:
     guessLanguage.info(str, function (languageInfo) {
@@ -186,9 +163,7 @@ var readTextAloud = function () {
         }
     });
 
-    // msg.lang = 'fr';
     msg.rate = 0.6;
-    speechSynthesis.cancel(); // try to address "freezing"; see https://stackoverflow.com/questions/21947730/chrome-speech-synthesis-with-longer-texts
     speechSynthesis.speak(msg)
 
 };
